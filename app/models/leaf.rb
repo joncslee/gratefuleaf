74897ebba17b02c4fb6cff4730471dfe25c056cc
Jonathan Lee <joncslee@gmail.com>
2012-12-31 02:56:11 -0500

    leaf validates presence of content

--- app/models/leaf.rb
+++ app/models/leaf.rb
@@ -1,6 +1,8 @@
 class Leaf < ActiveRecord::Base
   attr_accessible :content, :photo
 
+  validates_presence_of :content
+
   belongs_to :user
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
