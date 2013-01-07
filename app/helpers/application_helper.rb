ec08d7fa85b1ab1f9764877e209dbb33b9cd11cb
Jonathan Lee <joncslee@gmail.com>
2013-01-06 23:02:09 -0500

    helper for calculating daily/weekly goal percentage for progress bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  # calculates percentage of daily/weekly goal, to pass to progress bar
+  def goal_percentage
+
+    progress = 0
+    total = 1
+    percentage = 0
+
+    unless current_user.goal_type == User::NO_LEAF_GOAL
+      progress = current_user.goal_type == User::DAILY_LEAF_GOAL ? current_user.daily_leaves.size : current_user.weekly_leaves.size
+      total = current_user.goal_type == User::DAILY_LEAF_GOAL ? current_user.daily_leaf_goal : current_user.weekly_leaf_goal
+    end
+
+    percentage = (progress * 100) / total
+
+    percentage
+  end
 end
